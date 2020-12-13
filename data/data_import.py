0b0a47dd5d4bfcd0facfda52b128e9e372fcee12
MarionSauvage <marionsauvage@outlook.com>
2020-12-13 19:17:30 +0100

    data_import.py

--- data/data_import.py
+++ data/data_import.py
@@ -1,14 +1,23 @@
 import json
 import pandas as pd 
 import numpy as np 
-
-
+import random
 path="train.json"
-with open(path,'r',encoding='utf-8') as f:
-    data = json.loads(f.read()) 
-# Normalizing data
-multiple_level_data = pd.json_normalize(data.get('data'),['paragraphs'],'title')
-df_paragraphs = pd.json_normalize(data.get('data'),["paragraphs","qas"],["paragraphs",'title'])
-multiple_level_data=pd.DataFrame(multiple_level_data)
-df_global=pd.merge(df_paragraphs,multiple_level_data,on='title')
-print(df_global.sample())
+
+def import_context(path_context):
+    with open(path,'r',encoding='utf-8') as f:
+        data = json.loads(f.read()) 
+    # Normalizing data
+    multiple_level_data = pd.json_normalize(data.get('data'),['paragraphs'],'title')
+    multiple_level_data=pd.DataFrame(multiple_level_data)
+    list_contexts=list(multiple_level_data['context'])
+    return list_contexts
+
+def select_question(path_question):
+    with open(path,'r',encoding='utf-8') as f:
+        data = json.loads(f.read()) 
+    # Normalizing data
+    df_paragraphs = pd.json_normalize(data.get('data'),["paragraphs","qas"],["paragraphs",'title'])
+    liste_questions=list(df_paragraphs['questions'])
+    random_quest=random.randint(0,len(liste_questions))
+    return liste_questions[random_quest]
